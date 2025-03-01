4dc8daecb17394c15ef18116324707c31499a3ed
Lukas Gemela <lukasgemela45@gmail.com>
2025-03-01 20:44:34 +0100

    fixed entrypoint

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -5,11 +5,10 @@ composer install --ignore-platform-reqs
 npm install
 npm run build
 
-php artisan migrate
-php artisan db:seed
-
 if [ ! -f '.env' ]; then
    cp .env.example .env
 fi
+
+php artisan migrate:fresh --seed
 
 php-fpm
